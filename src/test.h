2955ec59a86dd7d74daa1f6b40e82b0cd01352af
Rene Molina <rdmolina@miners.utep.edu>
2023-04-19 23:05:05 -0600

    Update test.h

--- src/test.h
+++ src/test.h
@@ -11,8 +11,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-// #include <unistd.h>
-#include <signal.h>
+#include <unistd.h>
+// #include <signal.h>
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
