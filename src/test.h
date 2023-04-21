d4d7d98ec17a8230f7f2521ba10e021fb6adfc17
Rene Molina <rdmolina@miners.utep.edu>
2023-04-20 23:45:41 -0600

    Update test.h

--- src/test.h
+++ src/test.h
@@ -19,6 +19,12 @@
 #include <netdb.h>
 
 
+// Bank Transaction Types
+#define BANK_TRANS_DEPOSIT 0
+#define BANK_TRANS_WITHDRAW 1
+#define BANK_TRANS_INQUIRY 2
+
+
 // Client-side banking protocol
 typedef struct
 {
