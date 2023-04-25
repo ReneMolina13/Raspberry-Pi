6e8c3e4956f5c68bf6f3e2af327dcd0624b77e00
Rene Molina <rdmolina@miners.utep.edu>
2023-04-25 15:22:34 -0600

    Update test.c

--- src/test.c
+++ src/test.c
@@ -128,8 +128,8 @@ void test(char *ipAddr, NetStats *packetStats)
 {
 	while (1) {
 		for (int i = 0; i < NUM_PACKET_SIZES; i++) {
-			printf("Packet size: %i, ", (*packetStats)[i].packetSize);
-			printf("Iteration num: %lli\n", (*packetStats)[i].iteration);
+			printf("Packet size: %i, ", packetStats[i].packetSize);
+			printf("Iteration num: %lli\n", packetStats[i].iteration);
 		}
 		fputs("\n", stdout);
 	}
