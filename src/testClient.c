37229e82b1f32bac921a273c76cc5f317caa81be
Rene Molina <rdmolina@miners.utep.edu>
2023-04-25 15:28:33 -0600

    Update testClient.c

--- src/testClient.c
+++ src/testClient.c
@@ -374,6 +374,9 @@ int main(int argc, char **argv)
 		pthread_create(&args[i].tid, &attr, networkThreads, (void *) &args[i]);
 	}
 	
+	test(sockData.cmdIP, &packetStats);
+	
+	/*
 	// Fork process and call testing program
 	int pid = fork();
 	if (pid < 0) {
@@ -384,6 +387,7 @@ int main(int argc, char **argv)
 	else if (pid == 0) {
 		test(sockData.cmdIP, &packetStats);
 	}
+	*/
 	
 	// Wait for threads to terminate (only happens in case of error)
 	for (int i = 0; i < NUM_PACKET_SIZES; i++) {
