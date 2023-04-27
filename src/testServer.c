4e2e26e7ff29058b88d7d88ea5d0c281dde5a650
Rene Molina <rdmolina@miners.utep.edu>
2023-04-27 09:40:04 -0600

    Update testServer.c

--- src/testServer.c
+++ src/testServer.c
@@ -85,7 +85,7 @@ bool handleClient(int serverSocket)
 // TESTING
 //********************************************************************************************
 	printf("Received %li bytes from the client\n", bytesReceived);
-	printf("Data received: %s", buffer);
+	printf("Data received: %s\n", buffer);
 	printf("Sent %li bytes to the client\n\n", bytesSent);
 //********************************************************************************************
 	
