eddf362e8d2b9f28ed849db28cba585ed9188ce7
Rene Molina <rdmolina@miners.utep.edu>
2023-04-28 15:18:08 -0600

    Update test.c

--- src/test.c
+++ src/test.c
@@ -332,7 +332,7 @@ bool runIperf(char *hostname, int bandwidth, int numBytes, int interval, bool se
 				puts("7");
 			default:
 				printf("Error number: %i\n", errno);
-				stderr(errno);
+				fputs(stderr(errno), stderr);
 			}
 			return false;
 		 }
