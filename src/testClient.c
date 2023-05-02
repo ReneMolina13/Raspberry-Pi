4494e01e2bc13d13b933dedafc582d984d1328cf
Rene Molina <rdmolina@miners.utep.edu>
2023-05-02 04:10:06 -0600

    Update testClient.c

--- src/testClient.c
+++ src/testClient.c
@@ -268,7 +268,7 @@ bool formatOutput()
 		printf("%i", c);
 //*******************************************************************
 		
-	} while (c != EOF);
+	} while (c >= 0 && c <= 127);
 	pingResults.numTests = numRows / 5;
 	varsAssigned++;
 	rewind(pingFile);
